4c832d8eac09a3161681dd29dff358343b830066
yoratoni <yoratoni.dev@gmail.com>
2024-04-12 02:24:43 +0200

    Update

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,7 +2,17 @@ export default function Home() {
     return (
         <div className="flex flex-col items-center justify-center w-full min-h-screen text-white">
             <h1 className="text-5xl font-bold">React Boilerplate</h1>
-            <h4 className="text-xl font-semibold">by Yoratoni</h4>
+            <h4 className="text-xl font-semibold">
+                by&nbsp;
+                <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://github.com/yoratoni"
+                    className="font-semibold text-blue-500 hover:text-blue-400 active:text-blue-600"
+                >
+                    Yoratoni
+                </a>.
+            </h4>
         </div>
     );
 }
